7cbdd010e4531edc4cd76c1f66528b8fcf967dea
BhargavPabbaraju <bhargav.n.shyam@gmail.com>
2025-01-05 17:08:22 -0500

    Solve 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    if(years <0){
+        return "ERROR";
+    }
+    if(years % 100 === 0){
+        return years % 400 === 0;
+    }
+    return years % 4 === 0;
 };
 
 // Do not edit below this line
