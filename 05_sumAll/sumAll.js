8a2d810c29f38135c07e01cbdabac21a54407e85
BhargavPabbaraju <bhargav.n.shyam@gmail.com>
2025-01-05 17:06:10 -0500

    Solve 05_sumALl

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if(typeof a !== "number" || typeof b !=="number" || a<0 || b<0){
+        return "ERROR";
+    }
+    if(!Number.isInteger(a) || !Number.isInteger(b)){
+        return "ERROR";
+    }
+    const sumTillA = a*(a+1)/2;
+    const sumTillB = b*(b+1)/2;
+
+    return Math.abs(sumTillA - sumTillB) +1;
 
 };
 
